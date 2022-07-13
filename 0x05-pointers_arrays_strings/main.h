a91f4b2282a77e5ac93367b0b4ec2edd0d1a8a9c
Jude Ehimigbai <jjrufab@gmail.com>
2022-07-12 17:31:45 -0700

    edited

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,8 +1,9 @@
-#include <unistd.h>
-int _putchar(char);
+#include <stdlib.h>
+
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
-int _strlen(char *s);
+size_t _strlen(const char *str);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
@@ -11,4 +12,3 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
-void generationRandomPassword(int N);
